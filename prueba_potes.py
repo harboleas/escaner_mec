30272397760f88cd281ea2723b150b8de46850b0
Hugo Arboleas <harboleas@citedef.gob.ar>
2016-07-26 12:15:55 -0300

    add prueba_potes

---
+++ prueba_potes.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+#############################################
+# Prueba potes
+# ============
+#
+# Description :
+#  Prueba para recibir del arduino, los valores 
+#  de los potes
+#  
+# Author :
+#  Hugo Arboleas <harboleas@citedef.gob.ar>
+#
+#############################################
+
+import struct
+import serial
+import time
+
+# Reset del Arduino para una conexion limpia
+arduino = serial.Serial("/dev/ttyUSB0")
+arduino.setDTR(False)
+time.sleep(1)
+arduino.flushInput()
+arduino.setDTR(True)
+arduino.close()
+
+arduino = serial.Serial("/dev/ttyUSB0", 115200)
+
+def read_pote() :
+
+    datos = arduino.read(2)
+    return struct.unpack("h", datos)[0]  # convierte 2 bytes a int
+
+while True :
+
+    p1 = read_pote()
+    p2 = read_pote()
+    p3 = read_pote()
+    
+    print p1, p2, p3
+
+#  vim: set ts=8 sw=4 tw=0 et :
