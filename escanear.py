1e15bfa2e2575c2f1955eb7b15579aa5e14012de
Hugo Arboleas <harboleas@citedef.gob.ar>
2017-09-14 18:33:31 -0300

    pep8

--- escanear.py
+++ escanear.py
@@ -30,7 +30,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
-import FreeCAD 
+import FreeCAD
 from PySide import QtGui, QtCore
 import struct
 import serial
@@ -38,11 +38,11 @@ import time
 import math
 import Draft
 
-class Escaner_Dialog(QtGui.QDialog) :
+class Escaner_Dialog(QtGui.QDialog):
 
-    def __init__(self) :
+    def __init__(self):
         super(Escaner_Dialog, self).__init__()
-    
+
         self.RADIANES_POR_CUENTA = math.radians(66) / (552.0 - 257.0)
 
         doc = FreeCAD.ActiveDocument
@@ -60,21 +60,21 @@ class Escaner_Dialog(QtGui.QDialog) :
         self.pote3 = doc.Part__Feature004
         self.tuerca3 = doc.Part__Feature005
         self.brazo2 = doc.Body003
-        
+
         self.partes_rot1 = [self.acople, self.pote2, self.tuerca2, self.brazo1, self.pote3, self.tuerca3, self.brazo2]
         self.orig = [parte.Placement for parte in self.partes_rot1]
-        
+
         self.partes_rot1_orig = zip(self.partes_rot1, self.orig)
         self.partes_rot2 = self.partes_rot1[3:]
         self.partes_rot3 = self.partes_rot2[3:]
-        
-        
+
+
         self.rot1.Rotation.Axis = FreeCAD.Vector(0,0,1)
-        
+
         self.punto_orig = FreeCAD.Vector(0, -49.4, 3.9)
-       
+
         self.puntos = []
-        
+
         # Reset del Arduino para una conexion limpia
         self.arduino = serial.Serial("/dev/ttyUSB0")
         self.arduino.setDTR(False)
@@ -82,11 +82,11 @@ class Escaner_Dialog(QtGui.QDialog) :
         self.arduino.flushInput()
         self.arduino.setDTR(True)
         self.arduino.close()
-        
+
         self.arduino = serial.Serial("/dev/ttyUSB0", 115200)
-        
+
         self.arduino.read(1)  # espera que el arduino este listo
-        
+
         # Actualiza los datos cada 15 ms
         self.timer = QtCore.QTimer()
         self.connect(self.timer, QtCore.SIGNAL("timeout()"), self.update_escaner)
@@ -117,104 +117,105 @@ class Escaner_Dialog(QtGui.QDialog) :
         self.punto = None
         self.show()
 
-    def read_pote(self) :
-        
+    def read_pote(self):
+
         datos = self.arduino.read(2)
         return struct.unpack("h", datos)[0]  # convierte 2 bytes a int
-        
-    def update_escaner(self) :    # Actualiza el modelo segun los datos leidos
-        
+
+    def update_escaner(self):
+        "Actualiza el modelo segun los datos leidos"
+
         self.arduino.write("a") # pide los datos al arduino
 
-        K = self.RADIANES_POR_CUENTA       
+        K = self.RADIANES_POR_CUENTA
 
         # Lee el valor de los potes y lo convierte a radianes
-        p1 = self.read_pote() 
+        p1 = self.read_pote()
         p3 = self.read_pote()
         p2 = self.read_pote()
-        
+
         potes = "Cuentas potes : %d, %d, %d" % (p1, p2, p3)
         self.label_potes.setText(potes)
 
         offset1 = math.radians(0)
         offset2 = - math.radians(60)
         offset3 = - math.radians(100)
-        self.rotar(p1*K + offset1, p2*K + offset2, p3*K + offset3)    
+        self.rotar(p1*K + offset1, p2*K + offset2, p3*K + offset3)
 
         coord_punto = "Coord punto : %f, %f, %f" % (self.punto.x, self.punto.y, self.punto.z)
         self.label_punto.setText(coord_punto)
 
-        if self.adquirir.isChecked() :
+        if self.adquirir.isChecked():
             self.puntos.append(self.punto)
 
-    def tomar_coord(self) :
-        
+    def tomar_coord(self):
+
         Draft.makePoint(self.punto)
-        print self.punto     
+        print self.punto
         self.puntos.append(self.punto)
- 
-    def onOk(self) :
+
+    def onOk(self):
 
         self.timer.stop()
         self.arduino.close()
-#        if self.puntos :
+#        if self.puntos:
 #            aux = self.puntos[1:]
 #            aux.append(self.puntos[0])
 #            pares = zip(self.puntos, aux)
-#            for a,b in pares :
+#            for a,b in pares:
 #                Draft.makeLine(a, b)
-        
-        a = file("puntos.xyz", "w")
-        
-        for p in self.puntos :
+
+        a = open("puntos.xyz", "w")
+
+        for p in self.puntos:
             a.write("%f %f %f\n" % (p.x, p.y, p.z))
 
         a.close()
 
         # Reposicionar escaner al estado orig
 
-        for parte, orig in self.partes_rot1_orig :
+        for parte, orig in self.partes_rot1_orig:
             parte.Placement = orig
 
         self.close()
 
-    def rotar(self, ang1, ang2, ang3) :
+    def rotar(self, ang1, ang2, ang3):
 
         # Rotacion pote 1
         self.rot1.Rotation.Angle = ang1
-    
-        for parte, orig in self.partes_rot1_orig :
-            parte.Placement = self.rot1.multiply(orig)    
-    
+
+        for parte, orig in self.partes_rot1_orig:
+            parte.Placement = self.rot1.multiply(orig)
+
         punto = self.rot1.multVec(self.punto_orig)
-    
+
         # Rotacion pote 2
         z = FreeCAD.Vector(0,0,1)
         self.rot2.Rotation.Axis = self.pote2.Placement.Rotation.multVec(z)
         self.rot2.Rotation.Angle = ang2
         self.rot2.Base = self.pote2.Placement.Base
         despl = self.rot2.Base.negative()
- 
-        for parte in self.partes_rot2 :
+
+        for parte in self.partes_rot2:
             parte.Placement.move(despl)
-            parte.Placement = self.rot2.multiply(parte.Placement)    
- 
+            parte.Placement = self.rot2.multiply(parte.Placement)
+
         punto = punto.add(despl)
-        punto = self.rot2.multVec(punto)   
-        
+        punto = self.rot2.multVec(punto)
+
         # Rotacion pote 3
         self.rot3.Rotation.Axis = self.pote3.Placement.Rotation.multVec(z)
         self.rot3.Rotation.Angle = ang3
         self.rot3.Base = self.pote3.Placement.Base
         despl = self.rot3.Base.negative()
-    
-        for parte in self.partes_rot3 :
+
+        for parte in self.partes_rot3:
             parte.Placement.move(despl)
-            parte.Placement = self.rot3.multiply(parte.Placement)    
-    
+            parte.Placement = self.rot3.multiply(parte.Placement)
+
         punto = punto.add(despl)
-        punto = self.rot3.multVec(punto)   
-        self.punto = punto 
+        punto = self.rot3.multVec(punto)
+        self.punto = punto
 
 ###########################################################################################
 
